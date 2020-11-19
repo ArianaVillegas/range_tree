6b10bb40384ddd0cc7131f7fb4469b905f8719b9
ArianaVillegas <ariana.villegas@utec.edu.pe>
2020-11-18 20:38:45 -0500

    Update range tree

--- main.cpp
+++ main.cpp
@@ -7,20 +7,22 @@ using namespace std;
 int main() {
 
     srand(time(0));
-    int c1[13] = {34, 69, 39, 97, 20, 52, 37, 1, 84, 29, 82, 7, 4};
-    int c2[13] = {82, 6, 56, 42, 5, 12, 17, 25, 60, 57, 50, 55, 46};
+    int c1[20] = {23, 49, 41, 83, 51, 56, 61, 1, 32, 18, 59, 18, 24, 36, 80, 67, 67, 44, 48, 27};
+    int c2[20] = {89, 89, 80, 67, 36, 1, 13, 15, 21, 70, 99, 62, 99, 5, 25, 55, 16, 8, 79, 52};
     vector<Record> data;
-    for(int i=0; i<2000; i++){
-        int a = rand()%100;
-        int b = rand()%100;
+    for(int i=0; i<2000000; i++){
+        int a = rand()%10000;
+        int b = rand()%10000;
         data.push_back({{a,b},""});
         cout << a << " " << b << endl;
     }
+    data.push_back({{16,80},""});
+    data.push_back({{16,80},""});
     cout << "-------------------" << endl;
     RangeTree rt(data,2);
     //t.print();
 
-    auto ans = rt.range_query({{5,5},{55,100}});
+    auto ans = rt.range_query({{16,16},{80,80}});
     for(auto r : ans){
         for (auto c : r.coor)
             cout << c << ' ';
--- tree.h
+++ tree.h
@@ -24,8 +24,9 @@ struct Node{
     Node* parent;
     RangeTree* rangeTree;
     int max_leaf;
+    int depth;
     
-    Node(Record record, vector<Node*>::iterator itr){
+    Node(Record record, int depth, vector<Node*>::iterator itr){
         this->itr = itr;
         this->record = record;
         this->children[0] = 0;
@@ -33,6 +34,7 @@ struct Node{
         this->parent = 0;
         this->rangeTree = 0;
         this->max_leaf = 0;
+        this->depth = depth;
     }
 };
 
@@ -45,94 +47,63 @@ private:
     vector<Node*> records;
     Node* root;
 
-    Node* create_RangeTree(int l, int r){
+    Node* create_RangeTree(int l, int r, int depth){
+        Node *parent, *child1, *child2;
+        vector<int> coor(max_dim);
         if(l == r){
-            Node* parent = new Node(records[l]->record, records.end());
-            Node* child1 = records[l];
+            parent = new Node(records[l]->record, depth, records.end());
+            child1 = records[l];
+            child1->depth = depth+1;
             parent->children[0] = child1;
             parent->max_leaf = child1->record.coor[cur_dim];
             child1->parent = parent;
-
-            if(cur_dim+1 == max_dim) return parent;
-
-            // Create range tree
-            vector<Node*> subvector;
-            for(int i=l; i<=r; i++) subvector.push_back(records[i]);
-            parent->rangeTree = new RangeTree(subvector, max_dim, cur_dim+1);
-
-            return parent;
-        }
-        if(l+1 == r){
-            Node* child1 = records[l];
-            Node* child2 = records[r];
-            vector<int> coor(max_dim);
+        } else if(l+1 == r){
+            child1 = records[l];
+            child2 = records[r];
             coor[cur_dim] = child1->record.coor[cur_dim];
-            Node* parent = new Node({coor,""}, records.end());
+            parent = new Node({coor,""}, depth, records.end());
             parent->children[0] = child1;
             parent->children[1] = child2;
             parent->max_leaf = child2->record.coor[cur_dim];
-            child1->parent = parent;
-            child2->parent = parent;
-
-            if(cur_dim+1 == max_dim) return parent;
-
-            // Create range tree
-            vector<Node*> subvector;
-            for(int i=l; i<=r; i++) subvector.push_back(records[i]);
-            parent->rangeTree = new RangeTree(subvector, max_dim, cur_dim+1);
-
-            return parent;
+            child1->parent = child2->parent = parent;
+            child1->depth = child2->depth = depth + 1;
+        } else {
+            int m = (l + r) / 2;
+            child1 = create_RangeTree(l, m, depth + 1);
+            child2 = create_RangeTree(m + 1, r, depth + 1);
+            coor[cur_dim] = child1->max_leaf;
+            parent = new Node({coor, ""}, depth, records.end());
+            parent->children[0] = child1;
+            parent->children[1] = child2;
+            parent->max_leaf = child2->max_leaf;
+            child1->parent = child2->parent = parent;
+            child1->depth = child2->depth = depth + 1;
         }
-        int m = (l+r)/2;
-        Node* child1 = create_RangeTree(l, m);
-        Node* child2 = create_RangeTree(m+1, r);
-        vector<int> coor(max_dim);
-        coor[cur_dim] = child1->max_leaf;
-        Node* parent = new Node({coor,""}, records.end());
-        parent->children[0] = child1;
-        parent->children[1] = child2;
-        parent->max_leaf = child2->max_leaf;
-        child1->parent = parent;
-        child2->parent = parent;
-
-        if(cur_dim+1 == max_dim) return parent;
 
         // Create range tree
-        vector<Node*> subvector;
-        for(int i=l; i<=r; i++) subvector.push_back(records[i]);
-        parent->rangeTree = new RangeTree(subvector, max_dim, cur_dim+1);
+        if(cur_dim+1 != max_dim) {
+            vector<Record> subvector;
+            for (int i = l; i <= r; i++) subvector.push_back(records[i]->record);
+            parent->rangeTree = new RangeTree(subvector, max_dim, cur_dim + 1);
+        }
 
         return parent;
     }
 
 public:
     
-    RangeTree(vector<Record> data, int max_dim, int cur_dim=0){
+    RangeTree(vector<Record> &data, int max_dim, int cur_dim=0, int depth=0){
         sort(data.begin(), data.end(),[&cur_dim](const Record &r1, const Record &r2)->bool{
             return (r1.coor[cur_dim] < r2.coor[cur_dim]);
         });
         this->n = data.size();
         this->records.resize(n);
         for(int i=0; i<n; i++){
-            this->records[i] = new Node(data[i], records.begin()+i);
+            this->records[i] = new Node(data[i], depth, records.begin()+i);
         }
         this->max_dim = max_dim;
         this->cur_dim = cur_dim;
-        this->root = create_RangeTree(0, n-1);
-    }
-
-    RangeTree(vector<Node*> data, int max_dim, int cur_dim=0){
-        sort(data.begin(), data.end(),[&cur_dim](const Node* r1, const Node* r2)->bool{
-            return (r1->record.coor[cur_dim] < r2->record.coor[cur_dim]);
-        });
-        this->n = data.size();
-        this->records.resize(n);
-        for(int i=0; i<n; i++){
-            this->records[i] = new Node(data[i]->record, records.begin()+i);
-        }
-        this->max_dim = max_dim;
-        this->cur_dim = cur_dim;
-        this->root = create_RangeTree(0, n-1);
+        this->root = create_RangeTree(0, n-1, 0);
     }
 
     vector<Record> range_query(vector<pair<int,int>> ranges, int cur_dim=0){
@@ -143,13 +114,7 @@ public:
         for(int i=0; i<ranges.size(); i++){
 
             while(tmp->children[0]){
-                /*int c = tmp->record.coor[i];
-                int r_left = ranges[i].first;
-                if (r_left <= c)
-                    tmp = tmp->children[0];
-                else
-                    tmp = tmp->children[1];*/
-                tmp = tmp->children[(ranges[i].first>tmp->record.coor[i])];
+                tmp = tmp->children[(ranges[i].first > tmp->record.coor[i])];
             }
 
             auto itr = tmp->itr;
@@ -161,19 +126,35 @@ public:
 
                 auto left = tmp;
                 auto right = *itr;
-                while(left != right){
-                    left = left->parent;
-                    right = right->parent;
-                }
-                rt = left->rangeTree;
-                tmp = rt->root;
-            } else {
-                while(itr!=rt->records.end() and (*itr)->record.coor[i] <= ranges[i].second) {
-                    //TODO validar records
-                    rs.push_back((*itr)->record);
-                    itr++;
+                if(left != right) {
+                    while (left->depth < right->depth)
+                        right = right->parent;
+                    while (left->depth > right->depth)
+                        left = left->parent;
+                    while (left != right) {
+                        left = left->parent;
+                        right = right->parent;
+                    }
+
+                    rt = left->rangeTree;
+                    tmp = rt->root;
+                    continue;
                 }
             }
+
+            itr = tmp->itr;
+            while(itr!=rt->records.end() and (*itr)->record.coor[i] <= ranges[i].second) {
+                bool valid = 1;
+                for (int i = 0; i < ranges.size(); i++) {
+                    auto value = (*itr)->record.coor[i];
+                    if (ranges[i].first > value or value > ranges[i].second) {
+                        valid = 0;
+                    }
+                }
+                if (valid) rs.push_back((*itr)->record);
+                itr++;
+            }
+            break;
         }
 
         return rs;
